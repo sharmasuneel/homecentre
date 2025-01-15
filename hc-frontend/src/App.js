c9e103d9403c4d3bfadd1579e0bca6ce1929b515
RhythmAich <rhythm.a.aich@capgemini.com>
2025-01-15 21:34:27 +0530

    Added react-routing

--- hc-frontend/src/App.js
+++ hc-frontend/src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import Category from './pages/Category'
+import Login from './pages/Login'
+import Cart from './pages/Cart'
 import './App.css';
 import { Provider } from 'react-redux';
 import store from './redux/store'
@@ -6,7 +11,14 @@ import store from './redux/store'
 function App() {
   return (
     <Provider store={store}>
-      <h1>Home Center</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Cart" element={<Cart />} />
+        </Routes>
+      </Router>
     </Provider>
   );
 }
---
+++ hc-frontend/src/pages/OrderSummary.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OrderSummary = () => {
+  return (
+    <h1>OrderSummary</h1>
+  )
+}
+
+export default OrderSummary;
