3d276f3cb311aa80b066a17127961be5b661d2e9
Khadse <aniruddha-shantaram.khadse@capgemini.com>
2025-01-15 13:15:36 +0530

    Redux setup

---
+++ hc-frontend/src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+// Create the store with slices
+const store = configureStore({
+  reducer: {
+    // Create slices here
+  },
+});
+
+export default store;
